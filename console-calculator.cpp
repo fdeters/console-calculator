db243bb8a326bef5728ef0ef64593eacfd63437b
Forrest Deters <forrest.deters@gmail.com>
2022-03-21 10:41:38 -0700

    added basic functionality & catching zero division

--- console-calculator.cpp
+++ console-calculator.cpp
@@ -2,10 +2,37 @@
 //
 
 #include <iostream>
+#include "Calculator.h"
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    double x = 0.0;
+    double y = 0.0;
+    double result = 0.0;
+    char oper = '+';
+
+    cout << "Calculator Console Application\n\n";
+    cout << "Please enter the operation to perform. Format: a+b | a-b | a*b | a/b\n";
+    
+    Calculator c;
+    while (true)
+    {
+        cin >> x >> oper >> y;
+        if (oper == '/' && y == 0)
+        {
+            cout << "Division by 0 is impossible\n";
+            continue;
+        }
+        else
+        {
+            result = c.Calculate(x, oper, y);
+        }
+        cout << "Result is: " << result << "\n";
+    }
+    
+    return 0;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
